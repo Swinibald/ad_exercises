ff4b74a1983ed37f622023845d9342e5b69b0825
Swinibald <sven.stucki05@gmail.com>
2025-09-29 20:42:31 +0200

    refactor

--- src/main/java/ch/hslu/ad/Week2_2/MazeSolver.java
+++ src/main/java/ch/hslu/ad/Week2_2/MazeSolver.java
@@ -8,47 +8,47 @@ public class MazeSolver {
         this.maze = maze;
     }
 
-    public boolean findPath(int row, int column) {
+public boolean findPath(int row, int column) { 
 
-        if (row < 0 || column < 0 || row >= maze.length || column >= maze[row].length) {
-            return false;
-        }
-
-        if (maze[row][column] == '#' || maze[row][column] == '.') {
-            return false;
-        }
-
-        if (maze[row][column] == 'X') {
-            printMaze();
-            return true;
-        }
-
-        if (maze[row][column] == ' ') {
-            maze[row][column] = '.';
-            if (findPath(row + 1, column)) { // down
-                return true;
-            }
-            if (findPath(row - 1, column)) { // up
-                return true;
-            }
-            if (findPath(row, column + 1)) { // right
-                return true;
-            }
-            if (findPath(row, column - 1)) { // left
-                return true;
-            }
-
-            maze[row][column] = ' ';
-            return false;
-        }
+    if (row < 0 || column < 0 || row >= maze.length || column >= maze[row].length) {
         return false;
     }
 
+    if (maze[row][column] == '#' || maze[row][column] == '.') {
+        return false;
+    }
+    
+    if (maze[row][column] == 'X') {
+        printMaze();
+        return true; 
+    }
+
+    if (maze[row][column] == ' ') {
+        maze[row][column] = '.';
+        if (findPath(row + 1, column)) { // down
+            return true; 
+        }
+        if (findPath(row - 1, column)) { // up
+            return true;
+        }
+        if (findPath(row, column + 1)) { // right
+            return true;
+        }
+        if (findPath(row, column - 1)) { // left
+            return true;
+        }
+
+        maze[row][column] = ' ';
+        return false;
+    }
+    return false; 
+}
+
     private void printMaze() {
-        for (int i = 0; i < maze.length; i++) {
-            for (int j = 0; j < maze[i].length; j++) {
-                System.out.print(maze[i][j] + " ");
-            }
+    for (int i = 0; i < maze.length; i++) {
+        for (int j = 0; j < maze[i].length; j++) {
+            System.out.print(maze[i][j] + " ");
+                }
             System.out.println();
         }
         System.out.println();
@@ -56,18 +56,18 @@ public class MazeSolver {
 
     public static void main(String[] args) {
         char[][] mazeExample = {
-                { ' ', ' ', ' ', '#', '#', '#', '#', '#', '#', '#' },
-                { ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', '#', '#' },
-                { ' ', '#', ' ', '#', '#', '#', ' ', '#', ' ', '#' },
-                { ' ', '#', ' ', '#', ' ', ' ', ' ', '#', ' ', '#' },
-                { '#', '#', ' ', '#', '#', '#', ' ', '#', ' ', '#' },
-                { '#', '#', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ' },
-                { ' ', ' ', ' ', '#', '#', '#', '#', '#', '#', ' ' },
-                { ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' },
-                { ' ', '#', '#', '#', '#', '#', '#', ' ', '#', '#' },
+                { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' },
+                { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' },
+                { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' },
+                { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' },
+                { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' },
+                { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' },
+                { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' },
+                { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' },
+                { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' },
                 { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X' } };
 
         MazeSolver solver = new MazeSolver(mazeExample);
-        solver.findPath(0, 0);
+        solver.findPath(0,0);
     }
 }
